97ba3deca92c3508df554c8dec13f58bf250df3c
bitterbanks <bitterbanks@gmail.com>
2016-11-16 14:45:30 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -151,8 +151,6 @@
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
-    'Sean Rules'
-    'Mike is Lame'
     $('#age').html(p.age);
     $('#height').html(p.height);
     $('#systolicbp').html(p.systolicbp);
